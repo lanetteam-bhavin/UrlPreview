7f42868e176088554503ffd3c95d50fafc05e1a0
lanetteamkaushal <lanetteam.kaushlendra@gmail.com>
2016-02-08 18:24:50 +0530

    Commit before faster meta tag process.

---
+++ app/src/main/java/com/example/lcom53/urlpreview/WebCell.java
@@ -0,0 +1,19 @@
+package com.example.lcom53.urlpreview;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by lcom75 on 5/2/16.
+ */
+public class WebCell extends RelativeLayout {
+    int totalHeight = 0;
+
+    public WebCell(Context context) {
+        super(context);
+    }
+
+    public void setChatItem(MessageObject messageObject){
+
+    }
+}
